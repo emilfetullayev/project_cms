94615438bb985bcc9134e4d0c801efc554e59193
Emil Fetullayev <69794670+emilfetullayev@users.noreply.github.com>
2021-11-28 15:50:47 +0400

    Update category.php

--- category.php
+++ category.php
@@ -1,35 +1,25 @@
 <?php include 'includes/db.php' ?>
 <?php include 'includes/header.php' ?>
-
 <?php include 'includes/navigation.php' ?>
-
     <!-- Page Content -->
     <div class="container">
-
         <div class="row">
-
             <!-- Blog Entries Column -->
             <div class="col-md-8">
-
-
                 <?php
                      if(isset($_GET['category'])) {
                         $category = $_GET['category'];
                      }
-
                      $query = "SELECT * FROM posts WHERE post_category_id =  $category";
-
-                       $select_all_posts_query = mysqli_query($connection, $query);
-
+                     $select_all_posts_query = mysqli_query($connection, $query);
                       while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
                             $post_id = $row['post_id'];
-                           $post_title = $row['post_title'];
-                           $post_author = $row['post_author'];
-                           $post_date = $row['post_date'];
-                           $post_image = $row['post_image'];
-                           $post_content = $row['post_content'];
-                          ?>
-
+                            $post_title = $row['post_title'];
+                            $post_author = $row['post_author'];
+                            $post_date = $row['post_date'];
+                            $post_image = $row['post_image'];
+                            $post_content = $row['post_content'];
+                           ?>
 
                 <h1 class="page-header">
                     Page Heading
@@ -51,20 +41,11 @@
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
-
-                       <?php  } ?>
-
-
-                <!-- Second Blog Post -->
-         
-
+                       <?php  } ?>       
             </div>
 
         <?php include 'includes/sidebar.php' ?>
-
         </div>
-        <!-- /.row -->
-
         <hr>
 
  <?php include 'includes/footer.php' ?>
