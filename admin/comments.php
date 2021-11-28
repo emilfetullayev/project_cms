3e6df25e69755df5f42c04b5e207228d2daaf1e3
Emil Fetullayev <69794670+emilfetullayev@users.noreply.github.com>
2021-11-28 15:18:48 +0400

    Update comments.php

--- admin/comments.php
+++ admin/comments.php
@@ -1,57 +1,41 @@
 <?php include "includes/admin_header.php" ?>
 
-    <div id="wrapper">
-
-    
-
+    <div id="wrapper">   
     <?php include 'includes/admin_navigation.php' ?>
-
         <div id="page-wrapper">
-
             <div class="container-fluid">
-
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
                            <h1 class="page-header">
                             Welcome to Admin
                             <small>Author</small>
-                        </h1>
-                           
+                        </h1>                        
                       <?php
                           if(isset($_GET['source'])) {
-                            $source = $_GET['source'];
-                          
+                            $source = $_GET['source'];                         
                           }
                           else {
                             $source = '';
                           }
-
                           switch($source) {
                             case 'add_post';
                             include "includes/add_post.php";
-                            break;
-                     
+                            break;                   
                             case 'edit_post';
                             include "includes/edit_post.php";
                             break;
-
                             case '200';
                             echo "NICE 200";
                             break;
-
                             default:
-
                             include "includes/view_all_comments.php";
 
                             break;
                           }
 
                       ?>
-
-                
-                            
-              
+                                                         
                     </div>
                 </div>
                 <!-- /.row -->
