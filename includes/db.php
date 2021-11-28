0ddf7e770089368c6d99806a226fd9d6c9c28cbb
Emil Fetullayev <69794670+emilfetullayev@users.noreply.github.com>
2021-11-28 15:44:39 +0400

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $db['db_host'] = "localhost";
 $db['db_user'] = "root";
 $db['db_pass'] = "";
@@ -10,11 +9,7 @@ foreach ($db as $key => $value) {
 	define(strtoupper($key), $value);
 }
 
-
-
-
 $connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-
 
 if (!$connection) {
   die("Connection failed: " . mysqli_connect_error());
