d2e8010a1372393bc3b73c807250f94c15400b0a
Emil Fetullayev <69794670+emilfetullayev@users.noreply.github.com>
2021-11-28 15:43:44 +0400

    Update functions.php

--- admin/functions.php
+++ admin/functions.php
@@ -1,57 +1,52 @@
 <?php
 
 function comfirm($result) {
-    global $connection;
-   if(!$result) {
-      die("QUERY FAILED ." . mysqli_error($connection));
+       global $connection;
+         if(!$result) {
+         die("QUERY FAILED ." . mysqli_error($connection));
   }
-
 }
 
-  function insert_categories() {
-
+ function insert_categories() {
   	   global $connection;
 
            if(isset($_POST['submit'])) {
+             $cat_title = $_POST['cat_title'];                    
+             if(empty($cat_title)) {
+               echo "This field sholud not be em";
+             }else {
+               $query = "INSERT INTO categories(cat_title) VALUES ('$cat_title')";
 
-                                   $cat_title = $_POST['cat_title'];                    
-                                  if(empty($cat_title)) {
-                                    echo "This field sholud not be em";
-                                  }else {
-                                    $query = "INSERT INTO categories(cat_title) VALUES ('$cat_title')";
+               $select_category_query = mysqli_query($connection, $query);
 
-                                    $select_category_query = mysqli_query($connection, $query);
-
-                                    if(!$select_category_query) {
-                                        die('QUERY Failed' . mysqli_error($connection));
-                                    }
-                                  }
-                              }
-                         }
+               if(!$select_category_query) {
+                   die('QUERY Failed' . mysqli_error($connection));
+               }
+             }
+         }
+    }
 
 function findAllCategories(){
 	   global $connection;
 
-	     $query = "SELECT * FROM categories";
+	   $query = "SELECT * FROM categories";
                         
-          $select_categories_sidebar = mysqli_query($connection, $query);
+           $select_categories_sidebar = mysqli_query($connection, $query);
                                        
-          while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
-           $cat_id = $row['cat_id'];             
-              $cat_title = $row['cat_title'];
-                       
+           while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+             $cat_id = $row['cat_id'];             
+               $cat_title = $row['cat_title'];                     
                     echo "<tr>";
-                       echo "<td>{$cat_id}</td>";
-                         echo "<td>{$cat_title}</td>";
-                           echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-                            echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-                            echo "</tr>";
-                        }
+                      echo "<td>{$cat_id}</td>";
+                        echo "<td>{$cat_title}</td>";
+                         echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+                          echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+                           echo "</tr>";
+            }
 
-}
+      }
 
 function deleteCategories() {
-
          global $connection;
 	     if(isset($_GET['delete'])) {
            $the_cat_id = $_GET['delete']; 
