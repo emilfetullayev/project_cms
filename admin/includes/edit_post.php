186ac8b9a569e48b19fb665b6399c0e33dee4320
Emil Fetullayev <69794670+emilfetullayev@users.noreply.github.com>
2021-11-28 15:29:10 +0400

    Update edit_post.php

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,9 +1,8 @@
    <?php
              if(isset($_GET['p_id'])) {
                $the_post_id = $_GET['p_id'];
-
-}
-                       $query = "SELECT * FROM posts WHERE  post_id=' $the_post_id'";
+               }
+          $query = "SELECT * FROM posts WHERE  post_id=' $the_post_id'";
                         
           $select_posts_by_id = mysqli_query($connection, $query);
           $data = mysqli_fetch_assoc( $select_posts_by_id);
@@ -48,7 +47,6 @@
                 $RandomAccountNumber = uniqid();
                 move_uploaded_file($c_image_temp,  "../images/$ad");
                 
-
                 //If the image is empty, do not reset it
                 if(empty($post_image)) {
                   $query = "SELECT * FROM posts WHERE post_id =  $the_post_id";
@@ -74,52 +72,27 @@
         {
             echo 'not updated';
         }
-
-                   
-
             }
             // header( 'Location: edit_post.php' );
 
    ?>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   <form action="" method="post" enctype="multipart/form-data">    
-     
-     
+   <form action="" method="post" enctype="multipart/form-data">        
       <div class="form-group">
          <label for="title">Post Title</label>
           <input value="<?php echo  $data['post_title'];; ?>" type="text" class="form-control" name="title">
       </div>
-
-         <div class="form-group">
-    <!--         // Gonderilen deyer menimsedilir post_category_id'ye ve post bolumunde post_category_id, 
-            //category bolumunde'de cat_id ve cat_title update olunur, -->
+      <div class="form-group">
              <select name='post_categories' id=''>
                <?php
                  $query = "SELECT * FROM categories"; 
-
-                  $select_categories = mysqli_query($connection, $query);
+                 $select_categories = mysqli_query($connection, $query);
 
                   // confirmQuery($select_categories);
 
                    while ($row = mysqli_fetch_assoc($select_categories)) {
                            $cat_id = $row['cat_id'];             
-                           $cat_title = $row['cat_title'];
-                       
-
+                           $cat_title = $row['cat_title'];                     
                            echo "<option value='$cat_id'>{$cat_title}</option>";
                   }
 
@@ -130,51 +103,42 @@
 
        
        <div class="form-group">
-       <label for="title">Post Author</label>
-       <input value="<?php echo  $data['post_author']; ?>" type="text" class="form-control" name="author">       
-      
+             <label for="title">Post Author</label>
+             <input value="<?php echo  $data['post_author']; ?>" type="text" class="form-control" name="author">            
       </div>
          
         <div class="form-group">
     <!--    <label for="title">Post Author</label> -->
-       <input  type="hidden" class="form-control" name="post_id">       
-      
+             <input  type="hidden" class="form-control" name="post_id">            
       </div>
 
          <div class="form-group">
-       <label for="post_status">Post Status</label>
-       <input value="<?php echo  $data['post_status']; ?>" type="text"  class="form-control" name="post_status">       
-      
-      </div>
+              <label for="post_status">Post Status</label>
+              <input value="<?php echo  $data['post_status']; ?>" type="text"  class="form-control" name="post_status">            
+        </div>
 
       <div class="form-group">
-       <img src="../images/<?php echo $data['post_image']; ?>" style="width: 80px;">  
-        <input  type="file"  name="post_image">     
-      
-      </div>
-
-        <div class="form-group">
-       <label for="post_tags">Post Tags</label>
-       <input value="<?php echo  $data['post_tags']; ?>" type="text" class="form-control" name="post_tags">       
-      
-      </div>
-           <div class="form-group">
-       <label for="post_tags">Post Comment Count</label>
-       <input value="<?php echo  $data['post_comment_count']; ?>" type="text" class="form-control" name="post_comment_count">       
-      
+              <img src="../images/<?php echo $data['post_image']; ?>" style="width: 80px;">  
+              <input  type="file"  name="post_image">           
       </div>
 
        <div class="form-group">
-       <label for="post_content">Post Content</label>
-       <textarea class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo  $data['post_content'];; ?></textarea>      
-      
+               <label for="post_tags">Post Tags</label>
+               <input value="<?php echo  $data['post_tags']; ?>" type="text" class="form-control" name="post_tags">             
       </div>
-      
+      <div class="form-group">
+               <label for="post_tags">Post Comment Count</label>
+               <input value="<?php echo  $data['post_comment_count']; ?>" type="text" class="form-control" name="post_comment_count">            
+      </div>
 
        <div class="form-group">
-          <input class="btn btn-primary" type="submit" name="update_post" value="Publish Post">
-      </div>
-
+               <label for="post_content">Post Content</label>
+               <textarea class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo  $data['post_content'];; ?></textarea>            
+       </div>
+      
+       <div class="form-group">
+               <input class="btn btn-primary" type="submit" name="update_post" value="Publish Post">
+       </div>
 
 </form>
     
