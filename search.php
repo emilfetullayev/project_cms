96ba17680100addc3013f0b7a8727af48f16f5ed
Emil Fetullayev <69794670+emilfetullayev@users.noreply.github.com>
2021-11-28 15:54:32 +0400

    Update search.php

--- search.php
+++ search.php
@@ -1,25 +1,17 @@
 <?php include 'includes/db.php' ?>
 <?php include 'includes/header.php' ?>
-
 <?php include 'includes/navigation.php' ?>
 
     <!-- Page Content -->
     <div class="container">
-
         <div class="row">
-
             <!-- Blog Entries Column -->
             <div class="col-md-8">
-
-
                 <?php
-
                 	 if(isset($_POST['submit'])) {
             	 	 $search = $_POST['search'];
-
             	 	 $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
             	 	 $search_query = mysqli_query($connection, $query);
-
                      if(!$search_query) {
                      	die("QUERY FAILED" . mysqli_error($connection));
                      }
@@ -30,7 +22,6 @@
                      }
                      else {
                      	
-
                       while ($row = mysqli_fetch_assoc($search_query)) {
                            $post_title = $row['post_title'];
                            $post_author = $row['post_author'];
@@ -39,12 +30,10 @@
                            $post_content = $row['post_content'];
                           ?>
 
-
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
-
                 <!-- First Blog Post -->
                 <h2>
                     <a href="#"><?php echo "$post_title";?></a>
@@ -58,30 +47,18 @@
                 <hr>
                 <p><?php echo $post_content; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
                 <hr>
-
                        <?php  } 
                      }
             	 }
 
             	 ?>
-
-
-
-
-
-
-                <!-- Second Blog Post -->
-         
-
+                <!-- Second Blog Post -->        
             </div>
 
         <?php include 'includes/sidebar.php' ?>
-
         </div>
         <!-- /.row -->
-
         <hr>
 
  <?php include 'includes/footer.php' ?>
